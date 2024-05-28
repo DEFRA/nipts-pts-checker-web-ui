74d9a5f3c014734794bd3c9c3b332ff5bc0cf8ec
skcapgemini <121160206+skcapgemini@users.noreply.github.com>
2024-05-28 13:30:01 +0100

    test cases included with view story

---
+++ src/__tests__/web/helper/constants.test.js
@@ -0,0 +1,18 @@
+import constants from "../../../web/helper/constants";
+
+
+describe('Constants', () => {
+  it('should have correct registrationLink', () => {
+    expect(constants.registrationLink).toEqual('/v1/registration-idm2/postcode');
+  });
+
+  it('should have correct serviceVersion', () => {
+    expect(constants.serviceVersion).toEqual('2.0 Silver');
+  });
+
+  it('should have correct prototypeVersion', () => {
+    expect(constants.prototypeVersion).toEqual('v1');
+  });
+
+  // Add more tests for other properties as needed
+});
