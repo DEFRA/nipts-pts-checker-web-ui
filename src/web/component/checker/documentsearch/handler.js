47f5b35149fa9689a49fe3cc59341fb29e51310b
Dean Brown <dean.fm.brown@capgemini.com>
2024-06-27 11:29:51 +0100

    adding generic message for non 404 api errors, e.g 500 errors

--- src/web/component/checker/documentsearch/handler.js
+++ src/web/component/checker/documentsearch/handler.js
@@ -125,9 +125,9 @@ const submitSearch = async (request, h) => {
           });
         } else {
           return h.view(VIEW_PATH, {
-            error: responseData.error,
+            error: "An error occurred while processing your request.",
             errorSummary: [
-              { fieldId: "applicationNumberSearch", message: responseData.error },
+              { fieldId: "applicationNumberSearch", message: "An error occurred while processing your request."},
             ],
             activeTab: "application",
             formSubmitted: true,
