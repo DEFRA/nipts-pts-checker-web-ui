c518a313fc690cfa7d36d8bfdddcb9fcbb63ee52
Dean Brown <dean.fm.brown@capgemini.com>
2024-08-02 15:28:03 +0100

    adding in section value

--- src/web/component/checker/dashboard/handler.js
+++ src/web/component/checker/dashboard/handler.js
@@ -1,9 +1,11 @@
 "use strict";
 import DashboardMainModel from "../../../../constants/dashBoardConstant.js";
+import headerData from "../../../../web/helper/constants.js";
 
 const VIEW_PATH = "componentViews/checker/dashboard/dashboardView";
 
 const getDashboard = async (request, h) => {
+  headerData.section = "dashboard"
   const currentSailingSlot = request.yar.get("CurrentSailingSlot") || {};
   currentSailingSlot.currentDate = new Date().toLocaleDateString("en-GB");
   currentSailingSlot.pageTitle =
