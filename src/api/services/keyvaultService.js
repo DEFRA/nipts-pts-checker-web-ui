1ee07d576885d05c1691644a8e9d57cde02049c7
masafzal <masood.afzal@capgemini.com>
2024-08-02 12:26:10 +0100

    feat: Use Key vaults for secrets #41446 CP: Securely Store Keys and Configurations in Azure Vault and Update Frontend

---
+++ src/api/services/keyvaultService.js
@@ -0,0 +1,33 @@
+import { DefaultAzureCredential } from "@azure/identity";
+import { SecretClient } from "@azure/keyvault-secrets";
+import dotenv from "dotenv";
+
+// Load environment variables from .env file
+dotenv.config();
+
+if (process.env.NODE_ENV === "local" && !process.env.DEFRA_ID_CLIENT_ID) {
+  dotenv.config({ path: "./.env.local", override: true });
+}
+
+const getSecretClient = () => {
+  const keyVaultName = process.env.DEFRA_KEYVAULT_NAME;
+  const kvUri = `https://${keyVaultName}.vault.azure.net`;
+  const credential = new DefaultAzureCredential();
+  const client = new SecretClient(kvUri, credential);
+  return client;
+};
+
+const getSecretValue = async (secretName) => {
+  const client = getSecretClient();
+
+  try {
+    const secret = await client.getSecret(secretName);
+    return secret.value;
+  } catch (err) {
+    console.error("KeyVault: Error retrieving secret:", err);
+  }
+
+  return null;
+};
+
+export default getSecretValue;
--- src/config/auth.js
+++ src/config/auth.js
@@ -1,12 +1,22 @@
 import Joi from "joi";
 import dotenv from "dotenv";
+import getSecretValue from "../api/services/keyvaultService.js";
 
 // Load environment variables from .env file
 dotenv.config();
 
 if (process.env.NODE_ENV === "local" && !process.env.DEFRA_ID_CLIENT_ID) {
-  dotenv.config({ path: './.env.local', override: true});
+  dotenv.config({ path: "./.env.local", override: true });
 }
+
+const getConfigValue = async (envValue, secretKey) => {
+  if (!envValue || envValue === "") {
+    const kvValue = await getSecretValue(secretKey);
+    return kvValue;
+  }
+
+  return envValue;
+};
 
 const authSchema = Joi.object({
   defraId: {
@@ -24,18 +34,39 @@ const authSchema = Joi.object({
   },
 });
 
+const tenantName = await getConfigValue(
+  process.env.DEFRA_ID_TENANT,
+  "PTS-CP-B2C-TENANT"
+);
+const policy = await getConfigValue(
+  process.env.DEFRA_ID_POLICY,
+  "PTS-CP-B2C-POLICY"
+);
+const clientId = await getConfigValue(
+  process.env.DEFRA_ID_CLIENT_ID,
+  "PTS-CP-B2C-CLIENT-ID"
+);
+const clientSecret = await getConfigValue(
+  process.env.DEFRA_ID_CLIENT_SECRET,
+  "PTS-CP-B2C-CLIENT-SECRET"
+);
+const serviceId = await getConfigValue(
+  process.env.DEFRA_ID_SERVICE_ID,
+  "PTS-CP-B2C-SERVICE-ID"
+);
+
 const authConfig = {
   defraId: {
-    hostname: `https://${process.env.DEFRA_ID_TENANT}.b2clogin.com/${process.env.DEFRA_ID_TENANT}.onmicrosoft.com`,
+    hostname: `https://${tenantName}.b2clogin.com/${tenantName}.onmicrosoft.com`,
     oAuthAuthorisePath: "/oauth2/v2.0/authorize",
-    policy: process.env.DEFRA_ID_POLICY,
+    policy: policy,
     redirectUri: process.env.DEFRA_ID_REDIRECT_URI,
-    tenantName: process.env.DEFRA_ID_TENANT,
-    clientId: process.env.DEFRA_ID_CLIENT_ID,
-    clientSecret: process.env.DEFRA_ID_CLIENT_SECRET,
+    tenantName: tenantName,
+    clientId: clientId,
+    clientSecret: clientSecret,
     jwtIssuerId: process.env.DEFRA_ID_JWT_ISSUER_ID || "",
-    serviceId: process.env.DEFRA_ID_SERVICE_ID,
-    scope: `openid ${process.env.DEFRA_ID_CLIENT_ID} offline_access`,
+    serviceId: serviceId,
+    scope: `openid ${clientId} offline_access`,
     signOutUrl: process.env.DEFRA_ID_SIGNOUT_URI,
   },
 };
