069423aa6e3923a89f341cdb0dc85dd14a0d5a5a
Dean Brown <dean.fm.brown@capgemini.com>
2024-08-22 14:40:25 +0100

    Adding unit tests

--- src/__tests__/api/services/apiService.test.js
+++ src/__tests__/api/services/apiService.test.js
@@ -210,6 +210,206 @@ describe("apiService", () => {
       expect(result).toEqual(expectedInstance);
     });
 
+    it("should return transformed data when application number is valid and revoked", async () => {
+      const mockResponse = {
+        data: {
+          pet: {
+            petId: "1",
+            petName: "Buddy",
+            species: "Dog",
+            breedName: "Beagle",
+            microchipNumber: "123456789",
+            microchippedDate: "2022-01-01",
+            dateOfBirth: "2020-01-01",
+            sex: "Male",
+            colourName: "Brown",
+            significantFeatures: "None",
+          },
+          application: {
+            status: "revoked",
+            applicationId: "app123",
+            dateAuthorised: "2023-01-01",
+          },
+          travelDocument: {
+            travelDocumentReferenceNumber: "GB826TD123",
+            travelDocumentId: "td123",
+            dateOfIssue: "2023-02-01",
+          },
+        },
+      };
+
+      httpService.postAsync.mockResolvedValue({
+        status: 200,
+        data: mockResponse.data,
+      });
+      moment.mockImplementation((date) => ({
+        format: () => "01/01/2022",
+      }));
+
+      const result = await apiService.getApplicationByApplicationNumber(
+        "app123",
+        request
+      );
+      expect(httpService.postAsync).toHaveBeenCalledWith(
+        `${baseUrl}/Checker/checkApplicationNumber`,
+        { applicationNumber: "app123" },
+        request
+      );
+
+      const expectedInstance = new MicrochipAppPtdMainModel({
+        petId: "1",
+        petName: "Buddy",
+        petSpecies: "Dog",
+        petBreed: "Beagle",
+        ptdNumber: "GB826TD123",
+        documentState: "revoked",
+        microchipNumber: "123456789",
+        microchipDate: "01/01/2022",
+        petSex: "Male",
+        petDoB: "01/01/2022",
+        petColour: "Brown",
+        petFeaturesDetail: "None",
+        applicationId: "app123",
+        travelDocumentId: "td123",
+        dateOfIssue: "2023-02-01",
+      });
+
+      expect(result).toEqual(expectedInstance);
+    });
+
+    it("should return transformed data when application number is valid and rejected", async () => {
+      const mockResponse = {
+        data: {
+          pet: {
+            petId: "1",
+            petName: "Buddy",
+            species: "Dog",
+            breedName: "Beagle",
+            microchipNumber: "123456789",
+            microchippedDate: "2022-01-01",
+            dateOfBirth: "2020-01-01",
+            sex: "Male",
+            colourName: "Brown",
+            significantFeatures: "None",
+          },
+          application: {
+            status: "rejected",
+            applicationId: "app123",
+            dateAuthorised: "2023-01-01",
+          },
+          travelDocument: {
+            travelDocumentReferenceNumber: "GB826TD123",
+            travelDocumentId: "td123",
+            dateOfIssue: "2023-02-01",
+          },
+        },
+      };
+
+      httpService.postAsync.mockResolvedValue({
+        status: 200,
+        data: mockResponse.data,
+      });
+      moment.mockImplementation((date) => ({
+        format: () => "01/01/2022",
+      }));
+
+      const result = await apiService.getApplicationByApplicationNumber(
+        "app123",
+        request
+      );
+      expect(httpService.postAsync).toHaveBeenCalledWith(
+        `${baseUrl}/Checker/checkApplicationNumber`,
+        { applicationNumber: "app123" },
+        request
+      );
+
+      const expectedInstance = new MicrochipAppPtdMainModel({
+        petId: "1",
+        petName: "Buddy",
+        petSpecies: "Dog",
+        petBreed: "Beagle",
+        documentState: "rejected",
+        microchipNumber: "123456789",
+        microchipDate: "01/01/2022",
+        petSex: "Male",
+        petDoB: "01/01/2022",
+        petColour: "Brown",
+        petFeaturesDetail: "None",
+        applicationId: "app123",
+        travelDocumentId: "td123",
+        dateOfIssue: "2023-02-01",
+      });
+
+      expect(result).toEqual(expectedInstance);
+    });
+
+    it("should return transformed data when application number is valid and status is unknown", async () => {
+      const mockResponse = {
+        data: {
+          pet: {
+            petId: "1",
+            petName: "Buddy",
+            species: "Dog",
+            breedName: "Beagle",
+            microchipNumber: "123456789",
+            microchippedDate: "2022-01-01",
+            dateOfBirth: "2020-01-01",
+            sex: "Male",
+            colourName: "Brown",
+            significantFeatures: "None",
+          },
+          application: {
+            status:"",
+            applicationId: "app123",
+            dateAuthorised: "2023-01-01",
+          },
+          travelDocument: {
+            travelDocumentReferenceNumber: "GB826TD123",
+            travelDocumentId: "td123",
+            dateOfIssue: "2023-02-01",
+          },
+        },
+      };
+
+      httpService.postAsync.mockResolvedValue({
+        status: 200,
+        data: mockResponse.data,
+      });
+      moment.mockImplementation((date) => ({
+        format: () => "01/01/2022",
+      }));
+
+      const result = await apiService.getApplicationByApplicationNumber(
+        "app123",
+        request
+      );
+      expect(httpService.postAsync).toHaveBeenCalledWith(
+        `${baseUrl}/Checker/checkApplicationNumber`,
+        { applicationNumber: "app123" },
+        request
+      );
+
+      const expectedInstance = new MicrochipAppPtdMainModel({
+        petId: "1",
+        petName: "Buddy",
+        petSpecies: "Dog",
+        petBreed: "Beagle",
+        documentState: "",
+        microchipNumber: "123456789",
+        microchipDate: "01/01/2022",
+        petSex: "Male",
+        petDoB: "01/01/2022",
+        petColour: "Brown",
+        petFeaturesDetail: "None",
+        applicationId: "app123",
+        travelDocumentId: "td123",
+        dateOfIssue: "2023-02-01",
+      });
+
+      expect(result).toEqual(expectedInstance);
+    });
+
+
     it("should return error when application number is not found", async () => {
       httpService.postAsync.mockResolvedValue({
         status: 404,
@@ -277,6 +477,35 @@ describe("apiService", () => {
       httpService.postAsync.mockResolvedValue(mockError);
 
       const result = await apiService.recordCheckOutCome(checkOutcome);
+
+      expect(result).toEqual({ error: "Unexpected response structure" });
+    });
+  });
+
+  describe("saveCheckerUser", () => {
+    it("should return the summary id on success", async () => {
+      const checkOutcome = { applicationId: "app1", checkOutcome: "pass" };
+      const mockResponse = {
+        status: HttpStatusCode.OK,
+        data: { checkSummaryId: "summary1" },
+      };
+
+      httpService.postAsync.mockResolvedValue({
+        status: 200,
+        data: mockResponse.data,
+      });
+
+      const result = await apiService.saveCheckerUser(checkOutcome);
+
+      expect(result).toStrictEqual({"checkSummaryId": "summary1"});
+    });
+
+    it("should handle errors properly", async () => {
+      const checkOutcome = { applicationId: "app1", checkOutcome: "pass" };
+      const mockError = new Error("Test error");
+      httpService.postAsync.mockResolvedValue(mockError);
+
+      const result = await apiService.saveCheckerUser(checkOutcome);
 
       expect(result).toEqual({ error: "Unexpected response structure" });
     });
--- src/__tests__/api/services/microchipAppPtdMainService.test.js
+++ src/__tests__/api/services/microchipAppPtdMainService.test.js
@@ -72,6 +72,168 @@ describe("getMicrochipData", () => {
     expect(data).toEqual(expectedData);
   });
 
+  it("should fetch data and map it to MicrochipAppPtdMainModel with correct status mapping when approved", async () => {
+    const microchipNumber = "123456789012345";
+    const apiResponse = {
+      data: {
+        pet: {
+          petId: "715bb304-1ca8-46ba-552d-08dc28c44b63",
+          petName: "fido",
+          species: "Dog",
+          breedName: "Bulldog",
+          colourName: "White, cream or sand",
+          sex: "Male",
+          dateOfBirth: "2021-01-01T00:00:00",
+          microchippedDate: "2021-02-01T00:00:00",
+          significantFeatures: "None",
+        },
+        application: {
+          applicationId: "ae3d5e79-8821-47ae-5556-08dc295ccb5b",
+          referenceNumber: "SZWPFXEG",
+          dateOfApplication: "2024-02-09T11:31:29.7165377",
+          status: "approved",
+        },
+        travelDocument: {
+          travelDocumentId: "e385b94e-5d75-4015-611a-08dc295ccb0b",
+          travelDocumentReferenceNumber: "GB826J40C050",
+          dateOfIssue: "2024-06-12T10:26:52.0391239",
+        },
+      },
+    };
+
+    httpService.postAsync.mockResolvedValue(apiResponse);
+
+    const expectedData = new MicrochipAppPtdMainModel({
+      petId: "715bb304-1ca8-46ba-552d-08dc28c44b63",
+      petName: "fido",
+      petSpecies: "Dog",
+      petBreed: "Bulldog",
+      documentState: "approved",
+      ptdNumber: "GB826J40C050",
+      microchipNumber,
+      microchipDate: "01/02/2021", // Formatted date
+      petSex: "Male",
+      petDoB: "01/01/2021", // Formatted date
+      petColour: "White, cream or sand",
+      petFeaturesDetail: "None",
+      applicationId: "ae3d5e79-8821-47ae-5556-08dc295ccb5b",
+      travelDocumentId: "e385b94e-5d75-4015-611a-08dc295ccb0b",
+      dateOfIssue: "2024-06-12T10:26:52.0391239",
+    });
+
+    const data = await microchipApi.getMicrochipData(microchipNumber, request);
+
+    expect(data).toEqual(expectedData);
+  });
+
+  it("should fetch data and map it to MicrochipAppPtdMainModel with correct status mapping when revoked", async () => {
+    const microchipNumber = "123456789012345";
+    const apiResponse = {
+      data: {
+        pet: {
+          petId: "715bb304-1ca8-46ba-552d-08dc28c44b63",
+          petName: "fido",
+          species: "Dog",
+          breedName: "Bulldog",
+          colourName: "White, cream or sand",
+          sex: "Male",
+          dateOfBirth: "2021-01-01T00:00:00",
+          microchippedDate: "2021-02-01T00:00:00",
+          significantFeatures: "None",
+        },
+        application: {
+          applicationId: "ae3d5e79-8821-47ae-5556-08dc295ccb5b",
+          referenceNumber: "SZWPFXEG",
+          dateOfApplication: "2024-02-09T11:31:29.7165377",
+          status: "revoked",
+        },
+        travelDocument: {
+          travelDocumentId: "e385b94e-5d75-4015-611a-08dc295ccb0b",
+          travelDocumentReferenceNumber: "GB826J40C050",
+          dateOfIssue: "2024-06-12T10:26:52.0391239",
+        },
+      },
+    };
+
+    httpService.postAsync.mockResolvedValue(apiResponse);
+
+    const expectedData = new MicrochipAppPtdMainModel({
+      petId: "715bb304-1ca8-46ba-552d-08dc28c44b63",
+      petName: "fido",
+      petSpecies: "Dog",
+      petBreed: "Bulldog",
+      documentState: "revoked",
+      ptdNumber: "GB826J40C050",
+      microchipNumber,
+      microchipDate: "01/02/2021", // Formatted date
+      petSex: "Male",
+      petDoB: "01/01/2021", // Formatted date
+      petColour: "White, cream or sand",
+      petFeaturesDetail: "None",
+      applicationId: "ae3d5e79-8821-47ae-5556-08dc295ccb5b",
+      travelDocumentId: "e385b94e-5d75-4015-611a-08dc295ccb0b",
+      dateOfIssue: "2024-06-12T10:26:52.0391239",
+    });
+
+    const data = await microchipApi.getMicrochipData(microchipNumber, request);
+
+    expect(data).toEqual(expectedData);
+  });
+
+  it("should fetch data and map it to MicrochipAppPtdMainModel with correct status mapping when rejected", async () => {
+    const microchipNumber = "123456789012345";
+    const apiResponse = {
+      data: {
+        pet: {
+          petId: "715bb304-1ca8-46ba-552d-08dc28c44b63",
+          petName: "fido",
+          species: "Dog",
+          breedName: "Bulldog",
+          colourName: "White, cream or sand",
+          sex: "Male",
+          dateOfBirth: "2021-01-01T00:00:00",
+          microchippedDate: "2021-02-01T00:00:00",
+          significantFeatures: "None",
+        },
+        application: {
+          applicationId: "ae3d5e79-8821-47ae-5556-08dc295ccb5b",
+          referenceNumber: "SZWPFXEG",
+          dateOfApplication: "2024-02-09T11:31:29.7165377",
+          status: "rejected",
+        },
+        travelDocument: {
+          travelDocumentId: "e385b94e-5d75-4015-611a-08dc295ccb0b",
+          travelDocumentReferenceNumber: "GB826J40C050",
+          dateOfIssue: "2024-06-12T10:26:52.0391239",
+        },
+      },
+    };
+
+    httpService.postAsync.mockResolvedValue(apiResponse);
+
+    const expectedData = new MicrochipAppPtdMainModel({
+      petId: "715bb304-1ca8-46ba-552d-08dc28c44b63",
+      petName: "fido",
+      petSpecies: "Dog",
+      petBreed: "Bulldog",
+      documentState: "rejected",
+      ptdNumber: "SZWPFXEG",
+      microchipNumber,
+      microchipDate: "01/02/2021", // Formatted date
+      petSex: "Male",
+      petDoB: "01/01/2021", // Formatted date
+      petColour: "White, cream or sand",
+      petFeaturesDetail: "None",
+      applicationId: "ae3d5e79-8821-47ae-5556-08dc295ccb5b",
+      travelDocumentId: "e385b94e-5d75-4015-611a-08dc295ccb0b",
+      dateOfIssue: "2024-06-12T10:26:52.0391239",
+    });
+
+    const data = await microchipApi.getMicrochipData(microchipNumber, request);
+
+    expect(data).toEqual(expectedData);
+  });
+
   it("should return error when pet is not found", async () => {
     const microchipNumber = "123456789012345";
     const apiResponse = { error: { error: "Pet not found" } };
@@ -82,7 +244,7 @@ describe("getMicrochipData", () => {
 
     const data = await microchipApi.getMicrochipData(microchipNumber, request);
 
-    //expect(data).toEqual(expectedError);
+    expect(data).toEqual(expectedError);
   });
 
   it("should handle unexpected errors gracefully", async () => {
