42470e006cdd3edad07781deee6be7698268047e
Naga Gullipalli <naga.a.gullipalli@capgemini.com>
2024-08-28 09:14:20 +0100

    fis for error on sailing page

--- src/api/services/currentSailingMainService.js
+++ src/api/services/currentSailingMainService.js
@@ -2,6 +2,7 @@ import { CurrentSailingMainModel } from "../models/currentSailingMainModel.js";
 import CurrentSailingModel from "../../constants/currentSailingConstant.js";
 import dotenv from "dotenv";
 import httpService from "./httpService.js";
+import { HttpStatusConstants } from '../../../src/constants/httpMethod.js';
 
 dotenv.config();
 
@@ -16,13 +17,17 @@ const getCurrentSailingMain = async (request) => {
       request
     );
 
-    CurrentSailingModel.currentSailingMainModelData.routes = response.data.map(
-      (route) => ({
-        id: String(route.id), // Convert id to a string
-        value: route.routeName,
-        label: route.routeName,
-      })
-    );
+    if(response.status === HttpStatusConstants.OK && response.data !== undefined)
+    {
+      CurrentSailingModel.currentSailingMainModelData.routes = response.data.map(
+        (route) => ({
+          id: String(route.id), // Convert id to a string
+          value: route.routeName,
+          label: route.routeName,
+        })
+      );
+    }
+
     return new CurrentSailingMainModel(
       CurrentSailingModel.currentSailingMainModelData
     );
