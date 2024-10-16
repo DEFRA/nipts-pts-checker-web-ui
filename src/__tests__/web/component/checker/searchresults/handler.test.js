74f5e41bf5eff0a7f80262d4e1daa5ed8b3da203
Dean Brown <dean.fm.brown@capgemini.com>
2024-10-16 14:02:36 +0100

    fixing tests

--- src/__tests__/web/component/checker/searchresults/handler.test.js
+++ src/__tests__/web/component/checker/searchresults/handler.test.js
@@ -115,26 +115,24 @@ describe('saveAndContinueHandler', () => {
     }));
   });
 
-
-
-  it('should handle unexpected errors', async () => {
-    request.payload.checklist = 'Pass';
-    request.yar.get.mockReturnValueOnce({ documentState: 'active' });
-    validatePassOrFail.mockReturnValueOnce({ isValid: true });
-    apiService.recordCheckOutCome.mockRejectedValueOnce(new Error('Unexpected error'));
-
-    await SearchResultsHandlers.saveAndContinueHandler(request, h);
-
-    expect(h.view).toHaveBeenCalledWith('componentViews/checker/searchresults/searchResultsView', {
-      error: 'An error occurred while processing your request',
-      errorSummary: [{ fieldId: 'general', message: 'An unexpected error occurred' }],
-    });
-  });
-
   it('should return to document search if checks pass', async () => {
     request.payload.checklist = 'Pass';
-    request.payload.microchipNumber = "123456789012345"
-    request.yar.get.mockReturnValueOnce({ documentState: 'active' });
+    request.payload.microchipNumber = "123456789012345";
+    request.yar.get
+                .mockReturnValueOnce({ documentState: 'active' })
+                .mockReturnValueOnce({
+                  departureDate: '12/10/2023', // Format: DD/MM/YYYY
+                  sailingHour: '14',
+                  sailingMinutes: '30',
+                  selectedRoute: { id: 'route123' },
+                  selectedRouteOption: { id: 'option456' },
+                  routeFlight: 'FL1234',
+                });
+
+    apiService.recordCheckOutCome.mockResolvedValueOnce({
+      status: 'success', // You can set the status or result to whatever your function expects
+    });
+                
     validatePassOrFail.mockReturnValueOnce({ isValid: true });
 
     await SearchResultsHandlers.saveAndContinueHandler(request, h);
@@ -152,7 +150,21 @@ describe('saveAndContinueHandler', () => {
 
     expect(h.redirect).toHaveBeenCalledWith('/checker/non-compliance');
   });
-  
+
+  it('should handle unexpected errors', async () => {
+    request.payload.checklist = 'Pass';
+    request.yar.get.mockReturnValueOnce({ documentState: 'active' });
+    validatePassOrFail.mockReturnValueOnce({ isValid: true });
+    apiService.recordCheckOutCome.mockRejectedValueOnce(new Error('Unexpected error'));
+
+    await SearchResultsHandlers.saveAndContinueHandler(request, h);
+
+    expect(h.view).toHaveBeenCalledWith('componentViews/checker/searchresults/searchResultsView', {
+      error: 'An error occurred while processing your request',
+      errorSummary: [{ fieldId: 'general', message: 'An unexpected error occurred' }],
+    });
+  });
+
 });
 
 });
