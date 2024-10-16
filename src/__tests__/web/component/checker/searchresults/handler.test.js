ce3c4d5bea5525a5410fc044443ff3b1ac7195ed
Dean Brown <dean.fm.brown@capgemini.com>
2024-10-16 13:22:31 +0100

    adding additional coverage

--- src/__tests__/web/component/checker/searchresults/handler.test.js
+++ src/__tests__/web/component/checker/searchresults/handler.test.js
@@ -130,6 +130,29 @@ describe('saveAndContinueHandler', () => {
       errorSummary: [{ fieldId: 'general', message: 'An unexpected error occurred' }],
     });
   });
+
+  it('should return to document search if checks pass', async () => {
+    request.payload.checklist = 'Pass';
+    request.payload.microchipNumber = "123456789012345"
+    request.yar.get.mockReturnValueOnce({ documentState: 'active' });
+    validatePassOrFail.mockReturnValueOnce({ isValid: true });
+
+    await SearchResultsHandlers.saveAndContinueHandler(request, h);
+
+    expect(h.redirect).toHaveBeenCalledWith('/checker/document-search');
+  });
+
+  it('should return to non compliance if checks fail', async () => {
+    request.payload.checklist = 'Fail';
+    request.payload.microchipNumber = "123456789012345"
+    request.yar.get.mockReturnValueOnce({ documentState: 'active' });
+    validatePassOrFail.mockReturnValueOnce({ isValid: true });
+
+    await SearchResultsHandlers.saveAndContinueHandler(request, h);
+
+    expect(h.redirect).toHaveBeenCalledWith('/checker/non-compliance');
+  });
+  
 });
 
 });
