72576ac7777271e0c0005635f69f107785d82d44
skcapgemini <121160206+skcapgemini@users.noreply.github.com>
2024-11-01 16:49:38 +0000

    418653

---
+++ src/api/models/spsReferralMainModel.js
@@ -0,0 +1,12 @@
+class SpsReferralMainModel {
+  constructor(data) {
+    this.PTDNumber = data.PTDNumber;
+    this.PetDescription = data.PetDescription;
+    this.Microchip = data.Microchip;
+    this.TravelBy = data.TravelBy;
+    this.SPSOutcome = data.SPSOutcome;
+    this.classColour = data.classColour;
+  }
+}
+
+export { SpsReferralMainModel };
--- src/web/component/checker/referred/index.js
+++ src/web/component/checker/referred/index.js
@@ -1,4 +1,3 @@
-
 "use strict";
 
 import Routes from "./routes.js";
