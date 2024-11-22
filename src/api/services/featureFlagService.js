ffdf475a682eb976106848bdf2e3f7abf0459e8e
Dean Brown <dean.fm.brown@capgemini.com>
2024-11-22 16:17:48 +0000

    Adding feature flag management

---
+++ src/api/services/featureFlagService.js
@@ -0,0 +1,42 @@
+import { AppConfigurationClient } from "@azure/app-configuration";
+
+const connectionString = process.env.AZURE_APPCONFIG_CONNECTION_STRING;
+
+const client = new AppConfigurationClient(connectionString);
+
+const initializeFeatureFlags = async () => {
+  const featureFlags = new Map();
+
+  try {
+    for await (const setting of client.listConfigurationSettings({ keyFilter: ".appconfig.featureflag/*" })) {
+      const featureName = setting.key.replace(".appconfig.featureflag/", "");
+      featureFlags.set(featureName, JSON.parse(setting.value));
+    }
+
+    return featureFlags;
+  } catch (error) {
+    console.error("Error initializing feature flags:", error.message);
+    throw error; 
+  }
+};
+
+const isFeatureEnabled = async (featureName) => {
+  try {
+    const featureFlags = await initializeFeatureFlags(); // Initialize feature flags
+    const featureFlag = featureFlags.get(featureName);
+
+    // Check if the feature flag exists and if it's enabled (assuming the value is 'true' or 'enabled')
+    if (featureFlag && featureFlag.enabled === true) {
+      return true;
+    } else {
+      return false;
+    }
+  } catch (error) {
+    console.error("Error checking feature flag:", error.message);
+    return false;
+  }
+};
+
+export default {
+  isFeatureEnabled,
+};
