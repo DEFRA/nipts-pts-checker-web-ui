1ccd18f5e2668ac94720f2237c1977a6a3f3828c
Dean Brown <dean.fm.brown@capgemini.com>
2025-01-27 11:26:05 +0000

    Adjusting check to improve readability and not use an incomplete else if check

--- src/web/component/checker/referred/handler.js
+++ src/web/component/checker/referred/handler.js
@@ -122,17 +122,16 @@ const getReferredChecks = async (request, h) => {
 const postCheckReport = async (request, h) => {
   const { CheckSummaryId, PTDNumber, ApplicationNumber } = request.payload;
 
-   if (PTDNumber) {
-     request.yar.set("identifier", PTDNumber);
-   } 
-   else if (ApplicationNumber) {
-     request.yar.set("identifier", ApplicationNumber); 
-   } 
+  const identifier = PTDNumber || ApplicationNumber;
+  if (identifier) {
+    request.yar.set("identifier", identifier);
+  }
 
-    request.yar.set("checkSummaryId", CheckSummaryId);
+  request.yar.set("checkSummaryId", CheckSummaryId);
 
   return h.redirect("/checker/checkreportdetails");
 };
+
 
 
 
