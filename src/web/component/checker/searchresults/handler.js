3f850e4db51ac2b1777d6f21e1adde75fc9cba4f
skcapgemini <121160206+skcapgemini@users.noreply.github.com>
2025-02-25 13:45:27 +0000

    sonar

--- src/web/component/checker/searchresults/handler.js
+++ src/web/component/checker/searchresults/handler.js
@@ -6,24 +6,29 @@ import { CheckOutcomeConstants } from "../../../../constants/checkOutcomeConstan
 import { validatePassOrFail } from "./validate.js";
 
 const VIEW_PATH = "componentViews/checker/searchresults/searchResultsView";
+const PTD_LENGTH = 11;
+const PTD_PREFIX_LENGTH = 5;
+const PTD_MID_LENGTH = 8;
+
+const formatPtdNumber = (ptdNumber) => {
+  if (!ptdNumber) {
+    return "";
+  }
+
+  return (
+    `${ptdNumber.padStart(PTD_LENGTH, "0").slice(0, PTD_PREFIX_LENGTH)} ` +
+    `${ptdNumber
+      .padStart(PTD_LENGTH, "0")
+      .slice(PTD_PREFIX_LENGTH, PTD_MID_LENGTH)} ` +
+    `${ptdNumber.padStart(PTD_LENGTH, "0").slice(PTD_MID_LENGTH)}`
+  );
+};
 
 const getSearchResultsHandler = async (request, h) => {
   const microchipNumber = request.yar.get("microchipNumber");
   const data = request.yar.get("data");
 
-  const PTD_LENGTH = 11;
-  const PTD_PREFIX_LENGTH = 5;
-  const PTD_MID_LENGTH = 8;
-
-  data.ptdFormatted = data?.ptdNumber
-    ? `${data.ptdNumber
-        .padStart(PTD_LENGTH, "0")
-        .slice(0, PTD_PREFIX_LENGTH)} ` +
-      `${data.ptdNumber
-        .padStart(PTD_LENGTH, "0")
-        .slice(PTD_PREFIX_LENGTH, PTD_MID_LENGTH)} ` +
-      `${data.ptdNumber.padStart(PTD_LENGTH, "0").slice(PTD_MID_LENGTH)}`
-    : "";
+  data.ptdFormatted = data?.ptdNumber ? formatPtdNumber(data.ptdNumber) : "";
 
   const pageTitle = DashboardMainModel.dashboardMainModelData.pageTitle;
   let checklist = {};
@@ -37,11 +42,74 @@ const getSearchResultsHandler = async (request, h) => {
   return h.view(VIEW_PATH, { microchipNumber, data, pageTitle, checklist });
 };
 
+const handleValidationError = (request, h, validationResult) => {
+  const microchipNumber = request.yar.get("microchipNumber");
+  const data = request.yar.get("data");
+  const pageTitle = DashboardMainModel.dashboardMainModelData.pageTitle;
+
+  data.ptdFormatted = data?.ptdNumber ? formatPtdNumber(data.ptdNumber) : "";
+
+  return h.view(VIEW_PATH, {
+    error: validationResult.error,
+    errorSummary: [{ fieldId: "checklist", message: validationResult.error }],
+    microchipNumber,
+    data,
+    pageTitle,
+    formSubmitted: true,
+  });
+};
+
+const handleApiError = (request, h, checklist) => {
+  const errorMessage = errorMessages.serviceError.message;
+  const microchipNumber = request.yar.get("microchipNumber");
+  const data = request.yar.get("data");
+  const pageTitle = DashboardMainModel.dashboardMainModelData.pageTitle;
+
+  return h.view(VIEW_PATH, {
+    error: errorMessage,
+    errorSummary: [
+      {
+        fieldId: "unexpected",
+        message: errorMessages.serviceError.message,
+        dispalyAs: "text",
+      },
+    ],
+    microchipNumber,
+    data,
+    pageTitle,
+    formSubmitted: true,
+    checklist,
+  });
+};
+
+const prepareCheckOutcome = (request, data, checklist) => {
+  const currentSailingSlot = request.yar.get("currentSailingSlot") || {};
+  const currentDate = currentSailingSlot.departureDate
+    .split("/")
+    .reverse()
+    .join("-");
+  const dateTimeString = `${currentDate}T${currentSailingSlot.sailingHour}:${currentSailingSlot.sailingMinutes}:00Z`;
+
+  const isGBCheck = request.yar.get("isGBCheck");
+  const checkerId = request.yar.get("checkerId");
+
+  return {
+    applicationId: data.applicationId,
+    checkOutcome: checklist,
+    checkerId: checkerId,
+    routeId: currentSailingSlot?.selectedRoute?.id ?? null,
+    sailingTime: dateTimeString,
+    sailingOption: currentSailingSlot.selectedRouteOption.id,
+    flightNumber: currentSailingSlot.routeFlight || null,
+    isGBCheck: isGBCheck,
+  };
+};
+
 const saveAndContinueHandler = async (request, h) => {
   try {
     let { checklist } = request.payload;
-
     const data = request.yar.get("data");
+
     if (
       data.documentState === "rejected" ||
       data.documentState === "revoked" ||
@@ -52,86 +120,21 @@ const saveAndContinueHandler = async (request, h) => {
 
     const validationResult = validatePassOrFail(checklist);
     if (!validationResult.isValid) {
-      const microchipNumber = request.yar.get("microchipNumber");
-      const data = request.yar.get("data");
-      const pageTitle = DashboardMainModel.dashboardMainModelData.pageTitle;
-
-      const PTD_LENGTH = 11;
-      const PTD_PREFIX_LENGTH = 5;
-      const PTD_MID_LENGTH = 8;
-
-      data.ptdFormatted = data?.ptdNumber
-        ? `${data.ptdNumber
-            .padStart(PTD_LENGTH, "0")
-            .slice(0, PTD_PREFIX_LENGTH)} ` +
-          `${data.ptdNumber
-            .padStart(PTD_LENGTH, "0")
-            .slice(PTD_PREFIX_LENGTH, PTD_MID_LENGTH)} ` +
-          `${data.ptdNumber.padStart(PTD_LENGTH, "0").slice(PTD_MID_LENGTH)}`
-        : "";
-
-      return h.view(VIEW_PATH, {
-        error: validationResult.error,
-        errorSummary: [
-          { fieldId: "checklist", message: validationResult.error },
-        ],
-        microchipNumber,
-        data,
-        pageTitle,
-        formSubmitted: true,
-      });
+      return handleValidationError(request, h, validationResult);
     }
 
     if (checklist === CheckOutcomeConstants.Pass) {
-      const currentSailingSlot = request.yar.get("currentSailingSlot") || {};
-      const currentDate = currentSailingSlot.departureDate
-        .split("/")
-        .reverse()
-        .join("-");
-      const dateTimeString = `${currentDate}T${currentSailingSlot.sailingHour}:${currentSailingSlot.sailingMinutes}:00Z`;
-
-      const isGBCheck = request.yar.get("isGBCheck");
-      const checkerId = request.yar.get("checkerId");
-      const checkOutcome = {
-        applicationId: data.applicationId,
-        checkOutcome: checklist,
-        checkerId: checkerId,
-        routeId: currentSailingSlot?.selectedRoute?.id ?? null,
-        sailingTime: dateTimeString,
-        sailingOption: currentSailingSlot.selectedRouteOption.id,
-        flightNumber: currentSailingSlot.routeFlight || null,
-        isGBCheck: isGBCheck,
-      };
-
+      const checkOutcome = prepareCheckOutcome(request, data, checklist);
       const responseData = await apiService.recordCheckOutCome(
         checkOutcome,
         request
       );
 
       if (responseData?.error) {
-        const errorMessage = errorMessages.serviceError.message;
-        const microchipNumber = request.yar.get("microchipNumber");
-        const data = request.yar.get("data");
-        const pageTitle = DashboardMainModel.dashboardMainModelData.pageTitle;
-        return h.view(VIEW_PATH, {
-          error: errorMessage,
-          errorSummary: [
-            {
-              fieldId: "unexpected",
-              message: errorMessages.serviceError.message,
-              dispalyAs: "text",
-            },
-          ],
-          microchipNumber,
-          data,
-          pageTitle,
-          formSubmitted: true,
-          checklist,
-        });
+        return handleApiError(request, h, checklist);
       }
 
       request.yar.clear("IsFailSelected");
-
       request.yar.clear("routeId");
       request.yar.clear("routeName");
       request.yar.clear("departureDate");
