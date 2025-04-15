da74ad19f180669b3033b5e81d1c08ab2da2c02f
aismaelcap <x>
2025-04-15 16:02:11 +0100

    change route to /health

--- src/__tests__/web/component/checker/health/routes.test.js
+++ src/__tests__/web/component/checker/health/routes.test.js
@@ -1,4 +1,4 @@
-const HEALTH_CHECK_PATH = "/health-check";
+const HEALTH_CHECK_PATH = "/health";
 
 import Routes from "../../../../../web/component/checker/health/routes.js";
 import { HealthHandlers } from "../../../../../web/component/checker/health/handler.js";
@@ -7,7 +7,7 @@ import HttpMethod from "../../../../../constants/httpMethod.js";
 jest.mock("../../../../../web/component/checker/health/handler.js");
 
 describe("Health Routes", () => {
-  it("should define a GET route for /health-check", () => {
+  it("should define a GET route for /health", () => {
     const healthRoute = Routes.find(
       (route) =>
         route.path === HEALTH_CHECK_PATH && route.method === HttpMethod.GET
