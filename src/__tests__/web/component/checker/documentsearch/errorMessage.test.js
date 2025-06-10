a358f4e7a3fedaf880f65f584939ac9afa00c43f
aismaelcap <x>
2025-06-10 11:03:37 +0100

    fix broken unit tests

--- src/__tests__/web/component/checker/documentsearch/errorMessage.test.js
+++ src/__tests__/web/component/checker/documentsearch/errorMessage.test.js
@@ -5,25 +5,25 @@ describe("errorMessages", () => {
   test("should contain the correct error messages for PTD number", () => {
     expect(errorMessages.ptdNumber).toEqual({
       empty: "Enter a PTD number",
-      length: "Enter 6 characters after 'GB826'",
+      length: "PTD number must contain 6 characters after 'GB826'",
       invalid:
-        "Enter 6 characters after 'GB826', using only letters A to F and numbers",
+        "PTD number must contain 6 characters after 'GB826', using only letters A to F and numbers",
     });
   });
 
   test("should contain the correct error messages for application number", () => {
     expect(errorMessages.applicationNumber).toEqual({
       empty: "Enter an application number",
-      length: "Enter 8 characters",
-      invalid: "Enter 8 characters, using only letters and numbers",
+      length: "Application Number must be 8 characters long",
+      invalid: "Application Number must be 8 characters long, using only letters and numbers",
     });
   });
 
   test("should contain the correct error messages for microchip number", () => {
     expect(errorMessages.microchipNumber).toEqual({
       empty: "Enter a microchip number",
-      length: "Enter a 15-digit number",
-      invalid: "Enter a 15-digit number, using only numbers",
+      length: "Microchip Number must be 15 digits long",
+      invalid: "Microchip Number must be 15 digits long, using only numbers 0 to 9",
     });
   });
 });
