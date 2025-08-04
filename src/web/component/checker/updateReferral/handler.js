d6718c29c34827b6314a8b4ae5c7b2bef5bf8e4c
Dean Brown <dean.fm.brown@capgemini.com>
2025-08-04 17:19:17 +0100

    Adding updates referral view. Still need to add status colours + formatting, before moving onto submission logic and error checking

--- src/web/component/checker/updateReferral/handler.js
+++ src/web/component/checker/updateReferral/handler.js
@@ -1,11 +1,36 @@
 "use strict";
+import apiService from "../../../../api/services/apiService.js";
 
 const VIEW_PATH = "componentViews/checker/updateReferral/updateReferralView";
 
+
 const getUpdateReferralForm = async (request, h) => {
 
-  return h.view(VIEW_PATH);
+ const {
+        reference
+  } = request.payload;
+
+  const applicationData = await apiService.getApplicationByPTDNumber(
+      reference,
+      request
+  );
+
+  if (reference.startsWith("GB")) {
+      applicationData.PTDNumberFormatted = formatPTDNumber(reference);
+    }
+
+  return h.view(VIEW_PATH, { applicationData });
 };
+
+function formatPTDNumber(PTDNumber) {
+  const PTD_LENGTH = 11;
+  const PTD_PREFIX_LENGTH = 5;
+  const PTD_MID_LENGTH = 8;
+
+  return `${PTDNumber.padStart(PTD_LENGTH, "0").slice(0, PTD_PREFIX_LENGTH)} ` +
+      `${PTDNumber.padStart(PTD_LENGTH, "0").slice(PTD_PREFIX_LENGTH, PTD_MID_LENGTH)} ` +
+      `${PTDNumber.padStart(PTD_LENGTH, "0").slice(PTD_MID_LENGTH)}`;
+}
 
 const postUpdateReferralForm = async (request, h) => {
   /*const { CheckSummaryId, PTDNumber, ApplicationNumber } = request.payload;
--- src/web/component/checker/updateReferral/routes.js
+++ src/web/component/checker/updateReferral/routes.js
@@ -5,7 +5,7 @@ import HttpMethod from "../../../../constants/httpMethod.js";
 
 const Routes = [
   {
-    method: HttpMethod.GET,
+    method: HttpMethod.POST,
     path: "/checker/update-referral",
     options: {
       handler: UpdateReferralHandler.getUpdateReferralForm,
@@ -13,7 +13,7 @@ const Routes = [
   },
   {
     method: HttpMethod.POST,
-    path: "/checker/update-referral",
+    path: "/checker/update-referral-submit",
     options: {
       handler: UpdateReferralHandler.postUpdateReferralForm,
     },
