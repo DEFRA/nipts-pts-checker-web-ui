d9bbeec20d6770bc65bc363dd784cf6619b370b0
Dean Brown <dean.fm.brown@capgemini.com>
2025-08-07 12:38:21 +0100

    Adding unit test file

---
+++ src/__tests__/constants/updateReferralOutcomeConstant.test.js
@@ -0,0 +1,19 @@
+import { UpdateReferralErrors } from "../../constants/updateReferralOutcomeConstant.js";
+
+describe("UpdateReferralErrors", () => {
+  it("should contain the correct error messages", () => {
+    expect(UpdateReferralErrors.outcomeOptionError).toEqual(
+      "Select if the person is allowed or not allowed to travel under the Windsor Framework"
+    );
+    expect(UpdateReferralErrors.outcomeTextError).toEqual(
+      "Enter the reason you selected that outcome"
+    );
+    expect(UpdateReferralErrors.outcomeTextLengthError).toEqual(
+      "Outcome summary must be 500 characters or less"
+    );
+  });
+
+  it("should have exactly three error keys", () => {
+    expect(Object.keys(UpdateReferralErrors)).toHaveLength(3);
+  });
+});
